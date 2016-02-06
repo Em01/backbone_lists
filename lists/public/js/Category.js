ec6a2e0c3060d15d2bce7e2b18b87d304cda54d9
Em01 <emma_williams1@hotmail.co.uk>
2016-02-06 15:26:34 +0000

    Introduce Category Model.

---
+++ lists/public/js/Category.js
@@ -0,0 +1,15 @@
+window.Category = Backbone.Model.extend({
+  idAttribute: "_id",
+  url: function() {
+    if(this.get('_id'))
+      return '/category/' + this.get('_id');
+    else
+      return '/categories';
+  },
+
+  defaults: function() {
+    return {
+      name: "untitled"
+    };
+  }
+});
