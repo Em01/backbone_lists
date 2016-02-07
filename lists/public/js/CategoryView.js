e47da45dc5f35d0d2c6182b80698fb0d789a40a1
Em01 <emma_williams1@hotmail.co.uk>
2016-02-07 20:39:53 +0000

    Further work on view

--- lists/public/js/CategoryView.js
+++ lists/public/js/CategoryView.js
@@ -2,7 +2,8 @@ window.CategoryView = Backbone.View.extend({
   template: "#categoryView",
   events: {},
   initialize: function() {
-    _.bindAll(this, 'render'); 
+    _.bindAll(this, 'render');
+    this.collection.bind('all', this.render);
   },
 
   render: function() {
